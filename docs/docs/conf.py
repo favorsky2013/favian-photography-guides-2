95f4bd3b2a02aa3861860cd1ae3b40416d412245
favorsky2013 <106435411+favorsky2013@users.noreply.github.com>
2026-09-10 09:10:59 +1000

    Create conf.py

---
+++ docs/docs/conf.py
@@ -0,0 +1,7 @@
+project = "Favian Photography Guides"
+copyright = "2026, Favian Photography"
+author = "Favian Photography"
+extensions = []
+templates_path = []
+exclude_patterns = []
+html_theme = "alabaster"
