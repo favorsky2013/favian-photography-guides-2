91ff4cb0b9076977f29b5782414de9f8451f94bc
favorsky2013 <106435411+favorsky2013@users.noreply.github.com>
2026-09-10 09:15:28 +1000

    Delete docs/docs/docs/conf.py
    Remove misplaced configuration file

--- docs/docs/docs/conf.py
+++
@@ -1,7 +0,0 @@
-project = "Favian Photography Guides"
-copyright = "2026, Favian Photography"
-author = "Favian Photography"
-extensions = []
-templates_path = []
-exclude_patterns = []
-html_theme = "alabaster"
